6b98461764bdf49e61fa0131e1b4db9fc7d705da
Poleswarao <pmallela@employeemasterdatabase.com>
2024-08-31 10:13:56 +0530

    added reduxjs toolkit

--- src/redux/store/Store.jsx
+++ src/redux/store/Store.jsx
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Studentreducer } from "../reducers/StudentReducer";
 
 const rootReducer = combineReducers({
   student: Studentreducer,
 });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
+const store = configureStore({
+  reducer:rootReducer
+})
 
 export default store;
